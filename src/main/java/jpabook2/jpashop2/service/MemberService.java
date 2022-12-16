90b432cbcfa3a5c5488ce6241a68c99f480f76a7
gustjdw <johs710@gachon.ac.kr>
2022-12-16 17:53:59 +0900

    상품 도메인 개발

---
+++ src/main/java/jpabook2/jpashop2/service/MemberService.java
@@ -0,0 +1,52 @@
+package jpabook2.jpashop2.service;
+
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단일 조회
+     */
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+    /**
+     * 회원 이름 중복 검사 (but 별로)
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }  // findMembers size가 0보다 크면 예외처리도 좋음
+    }
+}
