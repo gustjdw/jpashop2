8842732de73657e3c70e8d24f97faab851be7d23
gustjdw <johs710@gachon.ac.kr>
2022-12-16 16:04:26 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook2/jpashop2/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook2.jpashop2.domain;
+
+import jpabook2.jpashop2.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
